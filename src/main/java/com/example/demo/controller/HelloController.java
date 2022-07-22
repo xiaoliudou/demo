e3b36d0f3296600feef5cd4a6d2cb184674b4749
dwchenyuhao_a <chenyuhao_a@aspirecn.com>
2022-07-22 17:55:49 +0800

    commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-    @GetMapping(value = "hello",name = "test")
+    @GetMapping(value = "hello",name = "index")
     public String index(@RequestParam(value = "name",required = false)String name){
         String result = "hello";
         return result;

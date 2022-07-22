b9c1124c3d6bc8482b0e5eb508e311fa9ec47fa7
dwchenyuhao_a <chenyuhao_a@aspirecn.com>
2022-07-22 17:54:21 +0800

    commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author chenyuhao_a
  */
 @RestController
+@RequestMapping("/test")
 public class HelloController {
-    @GetMapping(value = "hello",name = "test")
-    public String index(@RequestParam(value = "name",required = false)String name){
-        String result = "hello";
+    @GetMapping(value = "hello")
+    public String hello(@RequestParam(value = "name",required = false)String name){
+        String result = name;
         return result;
     }
 }

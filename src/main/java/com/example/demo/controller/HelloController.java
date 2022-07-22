a94d140c63f0085326a2b18676c64bf41008da2a
dwchenyuhao_a <chenyuhao_a@aspirecn.com>
2022-07-22 17:34:56 +0800

    commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenyuhao_a
+ */
+@RestController
+public class HelloController {
+    @GetMapping(value = "hello",name = "test")
+    public String index(@RequestParam(value = "name",required = false)String name){
+        String result = "hello";
+        return result;
+    }
+}
